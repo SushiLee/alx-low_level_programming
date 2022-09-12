13318d0834255b1b8ea4757f6b62ef20f218be70
Sushee <111068364+SushiLee@users.noreply.github.com>
2022-09-12 01:47:04 +0100

    Removed trailing whitespaces

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,9 +9,9 @@
 int main(void)
 {
 	int n, l;
-	
+
 srand(time(0));
-	
+
 	n = rand() - RAND_MAX / 2;
 	l = n % 10;
 	if (l > 5)
@@ -23,7 +23,7 @@ srand(time(0));
 		printf("Last digit of %d is %d and is 0\n", n, l);
 	}
 else
-	{	
+	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 	}
 	return (0);
