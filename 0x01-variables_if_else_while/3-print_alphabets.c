b9c65488a0b325f9778c87d7cd9818b73eeeea7e
SushiLee <naomidem122@gmail.com>
2022-09-11 17:23:38 -0700

    4

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,35 +3,26 @@
 
 
 /**
- *
- *  * main - Prints the alphabet.
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * main - Prints the alphabet.
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 
 {
 
-		char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	
+	int i;
+	for (i = 0; i < 52; i++)
 
-			int i;
+	{
 
+	putchar(alp[i])
+	}
 
+	putchar('\n');
 
-				for (i = 0; i < 52; i++)
-
-						{
-
-									putchar(alp[i]);
-
-										}
-
-					putchar('\n');
-
-						return (0);
+	return (0);
 
 }
