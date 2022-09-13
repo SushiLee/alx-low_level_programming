d5938bb659b21e78bada7eceb44ceb30d89c2d9a
Sushee <111068364+SushiLee@users.noreply.github.com>
2022-09-13 21:33:58 +0100

    8

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,21 +5,18 @@
   * Description: prints numbers 
   * Return: Always 0 (success) 
   */ 
- int main(void) 
- { 
- int i; 
-  
- for (i = '0'; i <= '9' ; i++) 
- { 
- putchar(i); 
-  
- if (i != '9') 
- { 
- putchar(','); 
- putchar(' '); 
- } 
- } 
-  
- putchar('\n'); 
- return (0); 
- }
+ int main(void)
+{
+	int num;
+	char letter;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
