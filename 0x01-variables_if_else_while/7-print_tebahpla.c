ae614680f39e3021bc4a3a9bbb5d0f3a7d57bcd5
Sushee <111068364+SushiLee@users.noreply.github.com>
2022-09-13 01:39:30 +0100

    Betty style

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,35 +1,21 @@
 #include <stdio.h>
 
-
-
 /**
- *
- *  * main - Prints reversed alphabet.
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * main - Prints reversed alphabet.
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 
 {
+int i;
 
-		int i;
+for (i = 122; i > 96; i--)
 
+	{
+	putchar(i);
+	}
 
-
-			for (i = 122; i > 96; i--)
-
-					{
-
-								putchar(i);
-
-									}
-
-				putchar('\n');
-
-					return (0);
-
+	putchar('\n');
+	return (0);
 }
