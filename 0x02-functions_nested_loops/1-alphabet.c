209e64d4ce9efbeb205ca544baed438be62e15c0
SushiLee <naomidem122@gmail.com>
2022-09-20 04:55:12 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Prints all alphabets in lowercase
+ * return - Always 0
+ */
+
+void print_alphabet(void);
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	putchar("/n");
+	return (0);
+}
