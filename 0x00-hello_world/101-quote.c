5b9e053e7a7e4c03b636b9dfaa64c54f98557c00
SushiLee <naomidem122@gmail.com>
2023-08-18 13:40:30 -0700

    Print statement without prinf or putchar

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,9 +8,9 @@
  */
 int main(void)
 {
-	const char *a = "and that piece of art is useful\";
+	const char *a = "and that piece of art is useful\"";
 
-	const char *b =  - Dora Korpar, 2015-10-19\n";
+	const char *b =  "- Dora Korpar, 2015-10-19\n";
 
 	write(2, a, strlen(a));
 	write(2, b, strlen(b));
