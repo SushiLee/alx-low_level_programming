650375ea31164bed176391d8c8738c479a719317
SushiLee <naomidem122@gmail.com>
2023-08-23 19:58:40 -0700

    Print lowercase then uppercase using ASCII

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 97; i <= 122; i++)
+	{
+                putchar(i);
+        }
+
+	for (j = 65; j <= 90; j++)
+	{	
+		putchar(j);
+	}	
+	{
+		putchar('\n');
+	}
+	return (0);
+}
