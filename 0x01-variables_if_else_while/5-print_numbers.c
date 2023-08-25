a209ff68ad884350b376fb1c48d7cd45148be224
SushiLee <naomidem122@gmail.com>
2023-08-25 15:51:12 -0700

    Prints single-digit numbers before 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single-digit numbers
+ * in base 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+		for(i = 0; i < 10; i++)
+		printf("%d", i);
+	return (0);
+}
+
