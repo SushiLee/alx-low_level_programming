650727e072e3a969b03bc0739c13696d476aeef3
Naomi Dem <naomidem122@gmail.com>
2023-08-26 12:35:34 +0100

    Hope this works

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,29 +1,31 @@
 #include <stdio.h>
 
 /**
-* main - Prints combo of digits till 89
-*
-* Return: Always 0 (Success)
-*/
+ * main - Prints digit combo till 89
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-  int i, j;
-  
-  for (i = 48; i <= 56; i++)
-  {
-    for (j = 49; j <= 57; j++)
-    {
-      putchar(i);
-      putchar(j);
+	int i,j;
 
-      if (i != 56 || j != 57)
-      {
-        putchar(',');
-        putchar(' ');
-      }
-    }
-  }
-  putchar('\n');
-  
-    return (0);
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 49; j <= 57; j++)
+		{
+			putchar(i);
+			putchar(j);
+
+			if (i != 57 || j != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	{
+		putchar('\n');
+	}
+
+	return (0);
 }
