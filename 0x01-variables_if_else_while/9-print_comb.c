f914cd7b897c37a3292bda3c7fedc1ad7e68abd2
Naomi Dem <naomidem122@gmail.com>
2023-08-26 11:43:40 +0100

    Print digit combo

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,12 +11,13 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i <= 57; i++)
+		for (i = 48; i <= 57; i++)
 		{
 			putchar(i);
 			if (i != 57)
 			{
 				putchar(',');
+				putchar(' ')
 			}
 		}
 	{
