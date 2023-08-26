d1306ebcc050a0e20a780504c0d0f49d64862912
Naomi Dem <naomidem122@gmail.com>
2023-08-26 13:14:57 +0100

    Betty style

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,28 +9,26 @@ int main(void)
 {
 	int i, j;
 	int printed[10][10] = {0};
-	
+
 	for (i = 0; i <= 9; i++)
-    {
-        for (j = i + 1; j <= 9; j++)
-        {
-            if (!printed[i][j])
-            {
-                putchar('0' + i);
-                putchar('0' + j);
-                
-                printed[i][j] = 1;
+	{
+        	for (j = i + 1; j <= 9; j++)
+		{
+			if (!printed[i][j])
+			{
+				putchar('0' + i);
+				putchar('0' + j);
+				printed[i][j] = 1;
 
-                if (i != 8 || j != 9)
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-        }
-    }
+				if (i != 8 || j != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
 
-    putchar('\n');
-
-    return 0;
+	return 0;
 }
