d0fe62a463fd4b3ebe4aca23177a80740ba700a5
Naomi Dem <naomidem122@gmail.com>
2023-08-28 02:12:51 +0100

    Code edits from codeblocks

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints 1 to 9
+ *
+ * Return: No Return
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0' + i);
+	}
+
